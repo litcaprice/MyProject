9cebcecc22f724c78a3d1344bc6b667f6bc08e36
litcaprice <91788432+litcaprice@users.noreply.github.com>
2022-02-17 22:11:16 +0300

    Update Horoskop.py

--- Horoskop.py
+++ Horoskop.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot("5225587018:AAHMQjOEwuvCgnfcuzFvaHNdh3sFnHCpNrU")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Привет")
+	bot.reply_to(message, "Пока")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
