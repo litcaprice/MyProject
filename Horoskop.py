fa463c86214f6901ac1988ce57946d39025faa56
litcaprice <91788432+litcaprice@users.noreply.github.com>
2022-02-17 21:43:41 +0300

    Add files via upload

---
+++ Horoskop.py
@@ -0,0 +1,13 @@
+import telebot
+
+bot = telebot.TeleBot("5225587018:AAHMQjOEwuvCgnfcuzFvaHNdh3sFnHCpNrU")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Привет")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
