50eb18f775f14b24a98854ecf9c8690d39408a9c
litcaprice <91788432+litcaprice@users.noreply.github.com>
2022-03-24 01:02:12 +0300

    Добавились новые диалоги для записи

--- tg.py
+++ tg.py
@@ -7,6 +7,7 @@ from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandle
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, Bot, keyboardbutton
 from datetime import datetime
 import mongodb
+import datetime
 
 def _init_(TELEGRAM_TOKEN):
     global bot
@@ -46,13 +47,26 @@ def mainLogic(Tg_cli: Update, context: CallbackContext) -> None:
                 Tg_cli.message.reply_text(f'{Tg_cli.effective_user.first_name}, select a button on the keyboard.')
         case 'signup':
             if text == 'Select date and time':
-                Tg_cli.message.reply_text(f'Okey')
+                continue_signup_date(Tg_cli, context)
             elif text == 'Write to the master':
                 Tg_cli.message.reply_text(f'@litcaprice')
             elif text == 'Back':
                 menu(Tg_cli, context)
+            elif text == 'Current entries':
+                Tg_cli.message.reply_text(f'meow')
             else:
                 Tg_cli.message.reply_text(f'{Tg_cli.effective_user.first_name}, select a button on the keyboard.')
+        case 'continue_signup_date':
+            if context.user_data['keyboard_date_enum'].get(text) != None:
+                continue_signup_time(Tg_cli, context)
+            else:
+                Tg_cli.message.reply_text(f'Pishi normalno, urod')
+        case 'continue_signup_time':
+            if context.user_data['keyboard_time_enum'].get(text) != None:
+                Tg_cli.message.reply_text(f'OK')
+            else:
+                Tg_cli.message.reply_text(f'Pishi normalno, urod')
+        
 
 def menu(Tg_cli: Update, context: CallbackContext) -> None:
     context.user_data['state'] = "menu"
@@ -77,14 +91,56 @@ def signup(Tg_cli: Update, context: CallbackContext) -> None:
     keyboard = [
         [
             KeyboardButton('Select date and time'),
+            KeyboardButton('Current entries')
             
         ],
         [
             KeyboardButton('Back'),
             KeyboardButton('Write to the master')
-        ]
+        ],
     ]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     text = Tg_cli.message.text
     Tg_cli.message.reply_text(f'Select huinu', reply_markup=reply_markup)
     
+def continue_signup_date(Tg_cli: Update, context: CallbackContext) -> None:
+    context.user_data['state'] = "continue_signup_date"
+    month = [
+        "january",
+        "february",
+        "march",
+        "april",
+        "may",
+        "june",
+        "july",
+        "august",
+        "september",
+        "october",
+        "november",
+        "december"
+    ]
+    now = datetime.datetime.now().date()
+    keyboard = [[0 for i in range(2)] for j in range(7)] # 7 lines, 2 in each 
+    keyboard_enum = {} # text date for checker in mainLogic
+    for i in range(14):
+        timestamp = now + datetime.timedelta(days=i)
+        text_date = str(timestamp.day) + " " + month[timestamp.month - 1] # forms text date from "PC Language" date
+        keyboard_enum[text_date] = timestamp # add text date to enum array
+        keyboard[i // 2][i % 2] = text_date # fills the keyboard
+    context.user_data['keyboard_date_enum'] = keyboard_enum
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    Tg_cli.message.reply_text(f'Select date', reply_markup=reply_markup)
+
+def continue_signup_time(Tg_cli: Update, context: CallbackContext) -> None:
+    context.user_data['state'] = "continue_signup_time"
+    now = datetime.datetime.now().date()
+    keyboard = [[0 for i in range(3)] for j in range(2)] # 2 lines, 3 in each 
+    keyboard_enum = {} # text date for checker in mainLogic
+    for i in range(6):
+        timestamp = datetime.timedelta(hours = 15 + i)
+        text_time = str(15 + i) + ":00" # forms text time from "PC Language" date
+        keyboard_enum[text_time] = timestamp # add text date to enum array
+        keyboard[i // 3][i % 3] = text_time # fills the keyboard
+    context.user_data['keyboard_time_enum'] = keyboard_enum
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    Tg_cli.message.reply_text(f'Select time', reply_markup=reply_markup)
